38667097acb01ccb6bbb467d5505c724b377784a
Callum Mann <cm13558@my.bristol.ac.uk>
2018-01-19 14:00:50 +0000

    updated timit with pickling and id dicts

---
+++ main.py
@@ -0,0 +1,7 @@
+from timit import TIMITDataset
+from vrae import VRAE
+
+if __name__ == "__main__":
+    timit = TIMITDataset()
+    # vrae = VRAE()
+    # vrae.train()
--- timit.py
+++ timit.py
@@ -4,14 +4,16 @@ from scipy.io import wavfile
 from sphfile import SPHFile
 import os
 import pprint
+import pickle
 
 class TIMITDataset(object):
     """/<CORPUS>/<USAGE>/<DIALECT>/<SEX><SPEAKER_ID>/<SENTENCE_ID>.<FILE_TYPE>"""
 
-    def __init__(self, base_path):
+    def __init__(self, base_path, force_reparse=False):
         self.base_path = base_path
         self.inner_path = "data/lisa/data/timit/raw/TIMIT"
         self.full_path = os.path.join(self.base_path, self.inner_path)
+        self.force_reparse = force_reparse
 
         self.dict_path = os.path.join(self.full_path, "DOC", "TIMITDIC.TXT")
         self.prompt_path = os.path.join(self.full_path, "DOC", "PROMPT.TXT")
@@ -47,9 +49,6 @@ class TIMITDataset(object):
                 phonemes = [x.strip().replace("/", "") for x in phonemes[1:-1].split()]
                 self.lex_word_count += Counter([word])
                 self.lex_phon_count += Counter(phonemes)
-        # for i, (phon, count) in enumerate(self.lex_phon_count.most_common()):
-        #     print(phon)
-        #     self.phoneme_to_id[phon] = i
 
     def _trans_word_and_phoneme_counts(self):
         # There are phonemes that do not occur in the lexicon therefore
@@ -57,9 +56,7 @@ class TIMITDataset(object):
         self.trans_phon_count = Counter()
         self.trans_word_count = Counter()
         for spkr, data in self.spkr_sents.items():
-            print(data)
             sents = [s_type + i for s_type, ids in data.items() for i in ids]
-            print(sents)
             for sent in sents:
                 _, wrds, phons = self.get_sentence_data(spkr, sent)
                 self.trans_word_count += Counter([wrd for _, _, wrd in wrds])
@@ -67,21 +64,29 @@ class TIMITDataset(object):
 
 
     def _parse_word_and_phon_ids(self):
-        self._lex_word_and_phoneme_counts()
-        self._trans_word_and_phoneme_counts()
-        self.all_phon_count = self.trans_phon_count + self.lex_phon_count
-        self.all_word_count = self.trans_word_count + self.lex_word_count
-        # self.word_id_to_phon_ids = {}
-        # self._lex_word_and_phoneme_ids()
-        # with open(self.dict_path, "r") as f:
-        #     for line in f:
-        #         if line[0] == ";":
-        #             continue
-        #         word, phonemes = line.split("  ")
-        #         phonemes = [x.strip().replace("/", "") for x in phonemes[1:-1].split()]
-        #         self.word_id_to_phon_ids[self.word_to_id[word]] = [
-        #             self.phoneme_to_id[phon] for phon in phonemes
-        #         ]
+        if os.path.exists("cache/") and not self.force_reparse:
+            with open("cache/all_word_counts.pickle", "rb") as f:
+                self.all_word_count = pickle.load(f)
+            with open("cache/all_phon_counts.pickle", "rb") as f:
+                self.all_phon_count = pickle.load(f)
+        else:
+            self._lex_word_and_phoneme_counts()
+            self._trans_word_and_phoneme_counts()
+            self.all_phon_count = self.trans_phon_count + self.lex_phon_count
+            self.all_word_count = self.trans_word_count + self.lex_word_count
+            os.mkdir("cache/")
+            with open("cache/all_word_counts.pickle", "wb") as f:
+                pickle.dump(self.all_word_count, f)
+            with open("cache/all_phon_counts.pickle", "wb") as f:
+                pickle.dump(self.all_phon_count, f)
+        self.word_to_id = {
+            word: i for i, (word, _) in enumerate(self.all_word_count.most_common())
+        }
+        self.phon_to_id = {
+            phon: i for i, (phon, _) in enumerate(self.all_phon_count.most_common())
+        }
+        self.pid_to_phon = {v, k for k, v in self.phon_to_id}
+        self.wid_to_word = {v, k for k, v in self.word_to_id}
 
     def _parse_spkr_sent(self):
         self.spkr_sents = {}
@@ -121,18 +126,12 @@ class TIMITDataset(object):
     def stats(self):
         return {
             "num_speakers": len(self.speakers),
-            "num_lexicon_words": len(self.lex_word_count.most_common()),
-            "num_lexicon_phonemes": len(self.lex_phon_count.most_common()),
-            "num_transcription_words": len(self.trans_word_count.most_common()),
-            "num_transcription_phonemes": len(self.trans_phon_count.most_common()),
             "num_all_words": len(self.all_word_count.most_common()),
             "num_all_phons": len(self.all_phon_count.most_common())
         }
 
     def _wav(self, file_path):
-        #sph file fix
-        print(file_path)
-        sample_rate, samples = wavfile.read(file_path)
+        return wavfile.read(file_path)
 
     def _wrd(self, file_path):
         wrd_list = []
@@ -174,5 +173,3 @@ if __name__ == "__main__":
     pp = pprint.PrettyPrinter(indent=4)
     pp.pprint(timit.stats())
     pp.pprint(timit.get_sentence_data("CJF0", "SA1"))
-    pp.pprint(timit.all_phon_count)
-    pp.pprint(timit.all_word_count)
